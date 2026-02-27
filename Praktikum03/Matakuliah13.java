91b442e1c4725fc944b703a4565fa8ecbe51c9f7
Isya-25th <isyaputra.r25th@gmail.com>
2026-02-27 08:40:36 +0700

    Add Matakuliah13

---
+++ Praktikum03/Matakuliah13.java
@@ -0,0 +1,15 @@
+package Praktikum03;
+
+public class Matakuliah13 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahjam;
+
+    public Matakuliah13(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahjam = jumlahJam;
+    }
+}
