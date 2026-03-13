2529c6f44936d0beea61284c90ef087cfd14a3ff
Isya-25th <isyaputra.r25th@gmail.com>
2026-03-13 08:58:10 +0700

    Add Praktikum 3

---
+++ BruteForceDivideConquer/minggu5/Sum.java
@@ -0,0 +1,32 @@
+package minggu5;
+
+public class Sum {
+    public int elemen;
+    public double keuntungan[];
+    public double total;
+
+    public Sum(int el) {
+        elemen = el;
+        keuntungan = new double[elemen];
+        total = 0;
+    }
+
+    public double totalBF() {
+        double total = 0;
+        for (int i = 0; i < keuntungan.length; i++) {
+            total = total + keuntungan[i];
+        }
+        return total;
+    }
+
+    public double totalDC(double arr[], int l, int r) {
+        if (l == r) {
+            return arr[l];
+        }
+        int mid = (l + r) / 2;
+        double lsum = totalDC(arr, l, mid);
+        double rsum = totalDC(arr, mid + 1, r);
+        
+        return lsum + rsum;
+    }
+}
