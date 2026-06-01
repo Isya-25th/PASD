ae913e7fb870f35e2dfddbccf6663c045c99958b
Isya-25th <isyaputra.r25th@gmail.com>
2026-06-02 01:47:46 +0700

    Add Percobaan 1

---
+++ Pertemuan14/Node13.java
@@ -0,0 +1,12 @@
+public class Node13 {
+    Mahasiswa13 mahasiswa;
+    Node13 left, right;
+
+    public Node13() {
+    }
+
+    public Node13(Mahasiswa13 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
